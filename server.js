454ef751bea2e35e45a4bbc9bc016b7991c7d20d
kimjl <jamesleekim90@gmail.com>
2019-05-09 19:05:39 -0400

    Initial QuotesApp commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// console.log('May Node be with you')
+
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+var db
+// Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password!
+MongoClient.connect('mongodb://testuser:abcd1234@ds231228.mlab.com:31228/quotesdb', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('quotesdb')
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('quotes').find().toArray(function(err, result) {
+    console.log(result)
+    res.render('index.ejs', {quotes: result})
+  })
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  });
+});
